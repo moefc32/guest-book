1f0dc5b22c11c54a0285b5caa7bfc9bc93337b03
Faizal Chan. <moefc32@gmail.com>
2026-06-28 07:00:24 +0700

    upgrade pnpm

--- apps/backend/source/app.js
+++ apps/backend/source/app.js
@@ -1,5 +1,10 @@
 import { IPBIND, PORT } from '$config';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import app from './route';
+
+const publicDirectory = path.join(process.cwd(), 'public');
+await fs.mkdir(publicDirectory, { recursive: true });
 
 app.listen({
     hostname: IPBIND,
